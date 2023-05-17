123a5749a674d4961c74b566451db92dd71667f9
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-05-17 20:41:02 +0100

    working, to do clients section

---
+++ project1/pages/clients/index.js
@@ -0,0 +1,11 @@
+const clientsPage = () => {
+    return (
+        <section>
+            <div>
+                <h1>This is the clients page</h1>
+            </div>
+        </section>
+    )
+}
+
+export default clientsPage;
