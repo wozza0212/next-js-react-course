5905d1181c09ed2554cc71115f9b0b5d7064a522
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-05-18 21:38:40 +0100

    added 404 page

---
+++ project1/pages/404.js
@@ -0,0 +1,13 @@
+const pageNotFound = () => {
+    return (
+        <section>
+            <div>
+                <h1>
+                    You seem to have stumbled upon a page which cannot be found!
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default pageNotFound;
